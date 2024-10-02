2242adb00e966f920945872372deddce43acc222
Karan Patil <79588593+karanpatil1604@users.noreply.github.com>
2024-10-02 11:32:13 +0530

    Made a minimal flask app

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+# http://localhost:5000/
+@app.route("/")
+def hello_world():
+    return "<h1>Hello, World!</h1>"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
