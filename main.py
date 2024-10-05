78e9b985b3aecfead22786f990b4def49a66b51b
Karan Patil <22f2001061@ds.study.iitm.ac.in>
2024-10-05 11:09:34 +0530

    Made a connection to database with SQLAlchemy and Defined a User Model

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template
 
+from db import db
+
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+db.init_app(app)
 
 
 # http://localhost:5000/
@@ -26,6 +31,8 @@ def list_books():
 
 
 if __name__ == "__main__":
+    # with app.app_context():
+    #     db.create_all()
     app.run(debug=True)
 
 
